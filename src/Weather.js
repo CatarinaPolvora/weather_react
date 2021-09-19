573223043ad570eeae6da033d86bad0f14279998
CatarinaPolvora <82556803+CatarinaPolvora@users.noreply.github.com>
2021-09-19 01:44:39 +0100

    fixed search on one click

--- src/Weather.js
+++ src/Weather.js
@@ -12,47 +12,50 @@ export default function Weather(props) {
   let [humidity, setHumidity] = useState(null);
   let [icon, setIcon] = useState(null);
   let [date, setDate] = useState(null);
-   let [city, setCity] = useState(props.city);
-   let [tempCity, setTempCity] = useState("");
-   let [coordinates, setCoordinates] = useState(null);
-  
+  let [city, setCity] = useState(props.city);
+  let [tempCity, setTempCity] = useState("");
+  let [coordinates, setCoordinates] = useState(null);
 
   function showTemperature(response) {
     setDescription(response.data.weather[0].description);
     setWind(response.data.wind.speed);
     setHumidity(response.data.main.humidity);
-    setIcon(
-      response.data.weather[0].icon
-    );
+    setIcon(response.data.weather[0].icon);
     setCoordinates(response.data.coord);
     setTemperature(response.data.main.temp);
     setDate(new Date(response.data.dt * 1000));
   }
-  
-function search(){
-let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=d4a6a488c953cc85e69ba334cc42f424&units=metric`;
+
+  function search(searchCity) {
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&appid=d4a6a488c953cc85e69ba334cc42f424&units=metric`;
     axios.get(url).then(showTemperature);
-}
+  }
 
   function handleSubmit(event) {
     event.preventDefault();
     setCity(tempCity);
-    
-    search();
+
+    search(tempCity);
   }
 
   function updateCity(event) {
-        setTempCity(event.target.value);}
+    setTempCity(event.target.value);
+  }
 
   if (temperature && date) {
     return (
       <body>
         <div>
-        <form onSubmit={handleSubmit}>
-        <input className="City" type="search" placeholder="Type a city" onChange={updateCity} />
+          <form onSubmit={handleSubmit}>
+            <input
+              className="City"
+              type="search"
+              placeholder="Type a city"
+              onChange={updateCity}
+            />
             <input className="Search" type="submit" value="Search" />
           </form>
-          
+
           <h1 className="neonText">{city}</h1>
           <span>
             Last updated: <FormattedDate date={date} />{" "}
@@ -60,10 +63,12 @@ let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=d4a6a
           <br />
           <div className="row">
             <div className="col">
-              <WeatherIcon mt-5 code={icon} size={120} className="icon"/>
+              <WeatherIcon mt-5 code={icon} size={120} className="icon" />
             </div>
             <div className="col">
-              <h2 className="neonText"><Temperature celsius={temperature}/></h2>
+              <h2 className="neonText">
+                <Temperature celsius={temperature} />
+              </h2>
               <h3 className="neonText">{description}</h3>
               <span>Humidity: {humidity}%</span> <br />
               <span>Wind: {wind} km/h</span>
@@ -76,8 +81,7 @@ let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=d4a6a
       </body>
     );
   } else {
-    
-    search();
+    search(city);
     return "Loading...";
   }
 }
--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -35,7 +35,7 @@ export default function WeatherForecast(props) {
       </div>
     );
   } else {
-    let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    let apiKey = "d4a6a488c953cc85e69ba334cc42f424";
     let longitude = props.coordinates.lon;
     let latitude = props.coordinates.lat;
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
